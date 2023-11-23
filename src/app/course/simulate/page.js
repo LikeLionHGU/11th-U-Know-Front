2e5bc72e410be8115d18e63e904e79c6ba4d62e2
Evvvaaaaan <vmfhrmfoald36@gmail.com>
2023-11-23 14:31:24 +0900

    simulate page.js work

--- src/app/course/simulate/page.js
+++ src/app/course/simulate/page.js
@@ -1,7 +1,19 @@
+"use client"
+
+import CustomTable from "@/components/CustomTable";
+import TextField from '@mui/material/TextField';
+
+
 export default function page() {
   return (
     <div>
-      <h6>수강 계획 시뮬레이션</h6>
+      <h3 style={{paddingBottom: "36px"}}>수강계획 시뮬레이션</h3>
+      <div style={{display: "flex"}}>
+        <div style={{display: "block", paddingBottom: "12px", paddingLeft: "0px"}}><TextField disabled defaultValue='세계관 1 2학점 필요'></TextField></div>
+        <div style={{display: "block", paddingBottom: "12px", paddingLeft: "8px"}}><TextField disabled defaultValue='세계관 1 2학점 필요'></TextField></div>
+        <div style={{display: "block", paddingBottom: "12px", paddingLeft: "8px"}}><TextField disabled defaultValue='세계관 1 2학점 필요'></TextField></div>
+      </div>
+      <CustomTable></CustomTable>
     </div>
   );
 }
