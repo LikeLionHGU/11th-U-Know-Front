a5847cde55d9b4272944df4094dc2c0adce4caf9
ohinhyuk <8156217@naver.com>
2023-12-02 03:04:20 +0900

    Feat: api 엔드포인트 정리

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -39,28 +39,24 @@ export const fetcher = async (args) => {
 // ----------------------------------------------------------------------
 
 export const endpoints = {
-  chat: "/api/chat",
-  kanban: "/api/kanban",
-  calendar: "/api/calendar",
   auth: {
-    me: "/api/auth/me",
-    login: "/api/auth/login",
-    register: "/api/auth/register",
+    signUp: "/auth/register",
+    signIn: "/auth/login",
   },
-  mail: {
-    list: "/api/mail/list",
-    details: "/api/mail/details",
-    labels: "/api/mail/labels",
-  },
-  post: {
-    list: "/api/post/list",
-    details: "/api/post/details",
-    latest: "/api/post/latest",
-    search: "/api/post/search",
-  },
-  product: {
-    list: "/api/product/list",
-    details: "/api/product/details",
-    search: "/api/product/search",
+  user: {
+    profile: "/user/get/info",
+    lecture: {
+      taken: "/user/get/lecture/list",
+      pass: "/user/get/lecture/subject",
+    },
+    like: "/user/subjectFavorites",
+    plan: {
+      addTable: "/user/addPlanTable",
+      addLecture: "/user/addPlanLecture",
+    },
+    graduate: {
+      list: "/user/PlanInfo",
+      // detail: '/user/get',
+    },
   },
 };
