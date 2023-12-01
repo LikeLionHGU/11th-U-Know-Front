98097d415ce623a2defd1ed6b40d09163b8f0ed9
ohinhyuk <8156217@naver.com>
2023-12-02 02:53:10 +0900

    Chore: axios instance ㅑ요ㅎ여 ㅑapi ㄱ ㅑ바 ㅑㅈㅑ

---
+++ src/utils/axios.js
@@ -0,0 +1,66 @@
+import axios, { AxiosRequestConfig } from "axios";
+// config
+
+// ----------------------------------------------------------------------
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+});
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  (error) =>
+    Promise.reject(
+      (error.response && error.response.data) || "Something went wrong"
+    )
+);
+
+axiosInstance.interceptors.request.use((config) => {
+  //    const token = localStorage.getItem('accessToken');
+  const token = process.env.NEXT_PUBLIC_TOKEN;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default axiosInstance;
+
+// ----------------------------------------------------------------------
+
+export const fetcher = async (args) => {
+  const [url, config] = Array.isArray(args) ? args : [args];
+
+  const res = await axiosInstance.get(url, { ...config });
+
+  return res.data;
+};
+
+// ----------------------------------------------------------------------
+
+export const endpoints = {
+  chat: "/api/chat",
+  kanban: "/api/kanban",
+  calendar: "/api/calendar",
+  auth: {
+    me: "/api/auth/me",
+    login: "/api/auth/login",
+    register: "/api/auth/register",
+  },
+  mail: {
+    list: "/api/mail/list",
+    details: "/api/mail/details",
+    labels: "/api/mail/labels",
+  },
+  post: {
+    list: "/api/post/list",
+    details: "/api/post/details",
+    latest: "/api/post/latest",
+    search: "/api/post/search",
+  },
+  product: {
+    list: "/api/product/list",
+    details: "/api/product/details",
+    search: "/api/product/search",
+  },
+};
