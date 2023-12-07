6dc5cdba4c4a11561e3c01717e83979da6906697
ohinhyuk <8156217@naver.com>
2023-12-07 20:48:57 +0900

    HotFix: 로컬스토리지 수정

--- src/utils/atom.js
+++ src/utils/atom.js
@@ -1,15 +1,15 @@
 import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
+// import { recoilPersist } from "recoil-persist";
 
-const { persistAtom } = recoilPersist({
-  key: "localStorage", //원하는 key 값 입력
-  storage: localStorage,
-});
+// const { persistAtom } = recoilPersist({
+//   key: "localStorage", //원하는 key 값 입력
+//   storage: localStorage,
+// });
 
 export const darkState = atom({
   key: "darkMode",
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  // effects_UNSTABLE: [persistAtom],
 });
 
 export const userState = atom({
@@ -25,7 +25,7 @@ export const userState = atom({
 export const isLoginState = atom({
   key: "isLoginState",
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  // effects_UNSTABLE: [persistAtom],
 });
 
 export const lectureUnTaken = atom({
